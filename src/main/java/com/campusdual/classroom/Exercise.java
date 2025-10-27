86cf51f4c424fe2fa2bc6cd2e6c8d87740d865c2
pablosandovalpita <pablo.sandoval@campusdual.com>
2025-10-27 09:06:34 +0100

    ejercicio 6

--- src/main/java/com/campusdual/classroom/Exercise.java
+++ src/main/java/com/campusdual/classroom/Exercise.java
@@ -3,22 +3,32 @@ package com.campusdual.classroom;
 public class Exercise {
 
     public static void main(String[] args) {
-        System.out.println("Sumar los 10 primeros números positivos.");
+        System.out.println("Sumar los 10 primeros números positivos:");
         sumFirstNaturalNumbers(10);
-        System.out.println("Mostrar los 20 primeros números naturales positivos.");
+        System.out.println("Mostrar los 20 primeros números naturales positivos:");
         showFirstNaturalNumbers(20);
     }
 
     //TODO ↓
     // Que imprima como mensaje la suma de los primeros N números positivos
     public static void sumFirstNaturalNumbers(int num) {
-
+        int total = 0;
+        for (int i = 0; i <= num; i++) {
+            total = total + i;
+        }
+        System.out.println("La suma desde 0 hasta " + num + " es " + total);
     }
 
     //TODO ↓
     // Que imprima por pantalla los N primeros números positivos
     public static void showFirstNaturalNumbers(int num) {
-
+        System.out.print("Números: ");
+        for (int i = 0; i <= num; i++) {
+            System.out.print(i);
+            if (i < num) {
+                System.out.print(", ");
+            }
+        }
+        System.out.println(); // salto de línea final
     }
-
 }
